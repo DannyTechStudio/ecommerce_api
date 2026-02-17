0cc320baf1f3514cf1395f4c0b4a6f159f045b3b
Daniel Offei <archimedes301@gmail.com>
2026-02-17 13:10:51 +0000

    feat(cart): add admin actions for expiring and extending carts using cart instance methods

--- cart/admin.py
+++ cart/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Cart, CartItem
 
 # Inline display for CartItem with Cart
@@ -17,6 +17,39 @@ class CartAdmin(admin.ModelAdmin):
     search_fields = ('id', 'user__first_name', 'user__email')
     readonly_fields = ('id', 'created_at', 'updated_at', 'expires_at', 'status')
     inlines = [CartItemInline]
+    
+    actions = ['mark_selected_carts_expired', 'extend_selected_carts']
+    
+    # Mark selected carts as expired
+    def mark_selected_carts_expired(self, request, queryset):
+        success_count = 0
+        for cart in queryset:
+            try:
+                cart.mark_expired()
+                cart.save()
+                success_count += 1
+            except  Exception as e:
+                self.message_user(request, f"Error expiring cart {cart.id}: {e}", level=messages.ERROR)
+        self.message_user(request, f"{success_count} cart(s) successfully marked as expired.")
+
+
+    mark_selected_carts_expired.short_description = "Mark selected carts as expired"
+
+
+    # Extend expiration by X minutes (example: 30)
+    def extend_selected_carts(self, request, queryset):
+        ttl_minutes = 30
+        success_count = 0
+        for cart in queryset:
+            try:
+                cart.extend_expiration(ttl_minutes)
+                cart.save()
+                success_count += 1
+            except Exception as e:
+                self.message_user(request, f"Error extending cart {cart.id}: {e}", level=messages.ERROR)
+        self.message_user(request, f"{success_count} cart(s) extended by {ttl_minutes} minutes.")
+
+    extend_selected_carts.short_description = "Extend selected carts expiration by 30 minutes"
    
     
 @admin.register(CartItem)
