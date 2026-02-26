b1b3106b134102987ceaa9fb1ca4e59215298b33
Daniel Offei <archimedes301@gmail.com>
2026-02-26 20:35:58 +0000

    feat(order): update order model to use UUID for its ID attribute

--- order/models.py
+++ order/models.py
@@ -51,6 +51,7 @@ class Order(models.Model):
     
     
 class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     product_id = models.UUIDField()
     product_name = models.CharField(max_length=200)
