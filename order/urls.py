926386f203ceab35777a03b289a9a721264f7563
Daniel Offei <archimedes301@gmail.com>
2026-03-09 00:21:13 +0000

    feat(order): configure URLS of the API endpoints of the order views

--- order/urls.py
+++ order/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import OrderDetailView, UserOrdersView
+from .views import (
+    UserOrdersView, 
+    OrderDetailView, 
+    CompleteOrderSerializer, 
+    CancelOrderView,
+    ShipOrderView,
+    DeliverOrderView,
+)
 
 urlpatterns = [
     path("", UserOrdersView.as_view(), name="user-orders"),
     path("<uuid:order_id>/", OrderDetailView.as_view(), name="order-detail"),
+    path("<uuid:order_id>/complete/", CompleteOrderSerializer.as_view(), name="complete-order"),
+    path("<uuid:order_id>/cancel/", CancelOrderView.as_view(), name="cancel-order"),
+    path("<uuid:order_id>/ship/", ShipOrderView.as_view(), name="ship-order"),
+    path("<uuid:order_id>/deliver/", DeliverOrderView.as_view(), name="deliver-order"),
 ]
 
