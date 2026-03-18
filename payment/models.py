df544b7878ada031bdb330d54b76adbfb784587d
Daniel Offei <archimedes301@gmail.com>
2026-03-18 21:37:18 +0000

    faet(payment): add enums for payment provider and channel choices and implement unique contraints for payments

--- payment/models.py
+++ payment/models.py
@@ -12,11 +12,25 @@ class PaymentStatus(models.TextChoices):
     SUCCESS = 'success', 'Success'
     FAILED = 'failed', 'Failed'
     REFUNDED = 'refunded', 'Refunded'
+    
+    
+# Payment Provider Enum
+class ProviderChoices(models.TextChoices):
+    PAYSTACK = "paystack", "Paystack"
+    
+    
+# Payment Channel Enum
+class ChannelChoices(models.TextChoices):
+    CARD = "card", "Card"
+    MOMO = "momo", "Momo"
+    BANK = "bank", "Bank"
 
 
 # Create your models here.
 class PaymentMethod(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    provider = models.CharField(max_length=50, choices=ProviderChoices.choices)
+    channel = models.CharField(max_length=50, choices=ChannelChoices.choices)
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=200, unique=True)
     is_active = models.BooleanField(default=True)
@@ -42,6 +56,12 @@ class Payment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["provider", "channel"],
+                name="unique_provider_channel"
+            )
+        ]
         ordering = ['-created_at']
         indexes = [
             models.Index(fields=["reference"]),
@@ -57,7 +77,7 @@ class Payment(models.Model):
 # Payment Event Model
 class PaymentEvent(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
-    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="events")
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="events", null=True, blank=True)
     event_type = models.CharField(max_length=100)
     payload = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
