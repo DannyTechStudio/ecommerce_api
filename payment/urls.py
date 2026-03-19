aceef89113aa8cf40105d8beb06f7b60fd2071c9
Daniel Offei <archimedes301@gmail.com>
2026-03-19 13:01:14 +0000

    feat(payment): add router for admin payment views to the urlpatterns

--- payment/urls.py
+++ payment/urls.py
@@ -1,12 +1,17 @@
+from rest_framework.routers import DefaultRouter
 from django.urls import path
 from .views import (
     PaymentMethodListView, 
     InitiatePaymentView, 
     VerifyPaymentView, 
-    PaymentDetailView
+    PaymentDetailView,
+    PaymentMethodAdminViewset
 )
 
 from .webhooks import PayStackWebhookView
+
+router = DefaultRouter()
+router.register(r'admin/payment-methods', PaymentMethodAdminViewset, basename='admin-payment-methods')
 
 urlpatterns = [
     path("methods/", PaymentMethodListView.as_view()),
@@ -15,3 +20,5 @@ urlpatterns = [
     path("<str:reference>/", PaymentDetailView.as_view()),
     path("webhook/paystack/", PayStackWebhookView.as_view(), name="paystack-webhook"),
 ]
+
+urlpatterns += router.urls
