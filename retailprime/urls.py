3966ab734b9ad369ed338880b3c45db0f04f5e71
Daniel Offei <archimedes301@gmail.com>
2026-03-19 15:31:29 +0000

    chore(urls): resolve order URLs issues as a result of incorrect keyword

--- retailprime/urls.py
+++ retailprime/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('api/auth/', include('accounts.urls')),
     path('api/', include('catalog.urls')),
     path('api/cart/', include('cart.urls')),
-    path('api/order/', include('order.urls')),
+    path('api/orders/', include('order.urls')),
     path('api/payment/', include('payment.urls')),
+    path('api/review/', include('review.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
