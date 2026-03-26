500866912350d474e647e12fdb403cc5d0cc4f3e
Daniel Offei <archimedes301@gmail.com>
2026-03-26 15:20:28 +0000

    feat(products): enable ordering product and product category by category name and name respectively

--- catalog/models.py
+++ catalog/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
+        ordering = ['name']
     
     def __str__(self):
         return self.name
@@ -32,6 +33,9 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['category__name', 'name']
     
     # Ensures 'is_active' is 'False' when quantity is zero
     def save(self, *args, **kwargs):
