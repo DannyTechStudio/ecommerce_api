4b95c5fe73215dd1feff8a394b213344b75dab56
Daniel Offei <archimedes301@gmail.com>
2026-04-25 17:46:35 +0000

    fix(payment/models): change the 'editable' method of the PaymentEvent ID field to False

--- payment/models.py
+++ payment/models.py
@@ -79,7 +79,7 @@ class Payment(models.Model):
     
 # Payment Event Model
 class PaymentEvent(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE, related_name="events", null=True, blank=True)
     event_type = models.CharField(max_length=100)
     payload = models.JSONField()
