a5a0f29d4173445d73abb9dbc21458e620e55c8c
Daniel Offei <archimedes301@gmail.com>
2026-04-29 16:17:06 +0000

    feat(wishlist): add wishlist app to enable user save product for later purchasing

---
+++ wishlist/models.py
@@ -0,0 +1,28 @@
+import uuid
+from django.db import models
+from django.conf import settings
+
+from catalog.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+
+# Create your models here.
+class WishList(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="wishlist")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.id} - {self.user}"
+    
+    
+class WishListItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=True)
+    wishlist = models.ForeignKey(WishList, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlist_items")
+    added_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.wishlist} - {self.product}"
+
